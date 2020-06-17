7f5fbbde77ae8e8fd3a08b5f39603c728b7926a1
evgar <eugene.garichev@gmail.com>
2020-06-17 12:31:45 +0300

    Updates

---
+++ precache-manifest.0f1efd63465a9e56e3f6cc2343d62513.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f56c2b6b72408b4bb7712b209429b915",
+    "url": "/twilio-video-app-react/index.html"
+  },
+  {
+    "revision": "cf0de64b579841bac9b0",
+    "url": "/twilio-video-app-react/static/js/2.d7140bcf.chunk.js"
+  },
+  {
+    "revision": "8dec489cc9fea776161524e157527d3e",
+    "url": "/twilio-video-app-react/static/js/2.d7140bcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18ca60668b6f513c555e",
+    "url": "/twilio-video-app-react/static/js/main.9083b3ef.chunk.js"
+  },
+  {
+    "revision": "1a674251b10223ee673f",
+    "url": "/twilio-video-app-react/static/js/runtime-main.2ff897fd.js"
+  },
+  {
+    "revision": "1b8c51ce350862c46a8e50112049fd52",
+    "url": "/twilio-video-app-react/static/media/google-logo.1b8c51ce.svg"
+  },
+  {
+    "revision": "0e04ea745e2c6f1fec53ca74a52de526",
+    "url": "/twilio-video-app-react/static/media/twilio-logo.0e04ea74.svg"
+  }
+]);
